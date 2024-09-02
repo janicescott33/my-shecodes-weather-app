5efb81d6966541c38b675f8d8923b71deeca9030
janicescott33 <janicescott33@icloud.com>
2024-09-02 12:42:41 +0100

    API functionality added

--- src/script.js
+++ src/script.js
@@ -1,9 +1,22 @@
-function changeCity(event) {
-  event.preventDefault();
-  let inputElement = document.querySelector("#search-city-input");
+function changeWeatherInfo(response) {
   let cityElement = document.querySelector("#weather-city");
-  cityElement.innerHTML = inputElement.value;
+  let temperatureElement = document.querySelector("#weather-temperature");
+  let temperature = response.data.temperature.current;
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function findCity(city) {
+  let apiKey = "b086a5a9f70a4d4df35ot83444fbbf55";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+  axios.get(apiUrl).then(changeWeatherInfo);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-city-input");
+  findCity(searchInput.value);
 }
 
 let formElement = document.querySelector("#search-city-form");
-formElement.addEventListener("submit", changeCity);
+formElement.addEventListener("submit", handleSubmit);
